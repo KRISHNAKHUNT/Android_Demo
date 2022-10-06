0a064d73973f7a06763b2460eec971d9ea9f0244
DELL <skothari902@rku.ac.in>
2022-10-06 13:09:39 +0530

    newww

--- app/src/main/java/com/example/database/MainActivity.java
+++ app/src/main/java/com/example/database/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
     public void screen(View view) {
         Intent i = new Intent(MainActivity.this, MainActivity3.class);
         startActivity(i);
+        //zxcvbnm
     }
 }
